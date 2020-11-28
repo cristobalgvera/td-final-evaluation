ab0cc822a19bc70f67c8737008e150ca31c9f76a
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-11-28 08:30:49 -0300

    repo init

---
+++ td-api/src/main/java/cl/crisgvera/tdapi/TdApiApplication.java
@@ -0,0 +1,13 @@
+package cl.crisgvera.tdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdApiApplication.class, args);
+    }
+
+}
---
+++ td-api/src/test/java/cl/crisgvera/tdapi/TdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.crisgvera.tdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
